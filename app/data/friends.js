57983c9b59363c81cad3ee38927bf79c422e18c8
Julie Nguyen <julienguyen86@yahoo.com>
2018-02-26 21:58:44 -0800

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,54 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [{
+        name: "Brad Pitt",
+        profilePic: "https://www.famousbirthdays.com/faces/pitt-brad-image.jpg",
+        scores: [
+
+            "4",
+            "5",
+            "5",
+            "2",
+            "4",
+            "5",
+            "4",
+            "4",
+            "3",
+            "4"
+        ]
+    },
+    {
+        name: "Angelina",
+        profilePic: "https://www.theplace2.ru/archive/angelina_jolie/img/james-white0061.jpg",
+        scores: [
+            "2",
+            "1",
+            "5",
+            "1",
+            "5",
+            "5",
+            "4",
+            "3",
+            "2",
+            "5"
+        ]
+    },
+    {
+        name: "Tom",
+        profilePic: "https://specials-images.forbesimg.com/imageserve/593b2e4b31358e03e55a0e8c/416x416.jpg?background=000000&cropX1=634&cropX2=2468&cropY1=39&cropY2=1874",
+        scores: [
+            "2",
+            "1",
+            "5",
+            "1",
+            "5",
+            "5",
+            "4",
+            "3",
+            "2",
+            "5"
+        ]
+    }
+]
+
+module.exports = friendList;
